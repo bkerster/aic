a4eee6afe8073b444cecf3eebf4143f6f33195a4
WarlordZap <warlordzap@gmail.com>
2014-06-09 18:00:21 -0700

    added mle

--- aicnew.py
+++ aicnew.py
@@ -1,6 +1,7 @@
 # aicnew: python style
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 def aicnew(timeSeries, plots, ssc):
     
@@ -12,11 +13,18 @@ def aicnew(timeSeries, plots, ssc):
     counts, plotvals = np.histogram(timeSeries, 50)
     
     distributions = ['normal', 'lognormal', 'exponential', 'pareto', 'gamma']
-    pdfs = [dict(name=dist) for dist in distributions]
+    #pdfs = [dict(name=dist) for dist in distributions]
+    pdfs = defaultdict(dict)
+    aicvals = defaultdict(dict)
     
     # calculate maximum likelihood for core distributions
     # calculate log likelihood value at maximum
     # find k (number of params)
     # generate probability density function using parameters
-    for i, pdf in enumerate(pdfs):
-        aicvals(i)
+    kvals = defaultdict(dict)
+    for dist in distributions:
+        aicvals[dist]['mle'] = aicmle(timeSeries, dist)
+        aicvals[dist]['nll'] = aiclike(timeSeries,aicvals[dist]['mle']['params'], dist)
+        kvals[dist]['vals'] = aicpdf(plotvals, dist, aicvals[dist]['mle']['params'])
+    
+    
